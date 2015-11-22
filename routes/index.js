cfcee72aa6bede5f1e982a3c7c01f69d6199b477
faizanv <faizan.virani.44@gmail.com>
2015-11-22 12:04:32 -0600

    Ship it

--- routes/index.js
+++ routes/index.js
@@ -4,17 +4,9 @@ var request = require('request');
 var Firebase = require("firebase");
 var fb = new Firebase("https://pushingbuttons.firebaseio.com/");
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  fb.on('value', function(dataSnapshot) {
-    console.log(dataSnapshot.val());
-    res.render('index', { title: dataSnapshot.val().mode });
-  });
-});
-
-router.get('/orderFood', function(req, res, next) {
+function orderFood(callback) {
   request.post('https://api.postmates.com/v1/customers/cus_K_4gGQ9MY-HkOF/deliveries', function(error, response, body) {
-    res.json(JSON.parse(body));
+    callback(JSON.parse(body));
   })
   .auth('9237cd55-54ed-472b-9cd9-7acfc2af3e1b', '', true)
   .form({
@@ -26,23 +18,63 @@ router.get('/orderFood', function(req, res, next) {
     dropoff_phone_number: "9546097527",
     manifest: "Burrito with chicken, brown rice, black beans, cheese, and sour cream"
   });
-});
+}
 
-router.get('/morning', function(req, res, next) {
+function morning(callback) {
   request.post('https://api.twilio.com/2010-04-01/Accounts/ACacc9d594b831d7af5208868049ddb9e9/Messages.json', function(error, reponse, body) {
-    res.json(JSON.parse(body));
+    callback(JSON.parse(body));
   })
   .auth("ACacc9d594b831d7af5208868049ddb9e9", "53c50aa776653ae1ccdc749d131d80df", true)
   .form({
-    To: "9546097527",
+    To: "4252313721",
     From: "+14692511032",
-    Body: "Good morning beautiful <3"
+    Body: "Good morning beautiful <3",
+    MediaUrl: "http://i.imgur.com/68EexTU.jpg"
+  });
+}
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  fb.on('value', function(dataSnapshot) {
+    console.log(dataSnapshot.val());
+    res.render('index', { title: dataSnapshot.val().mode });
+    fb.off();
   });
 });
 
-router.post("/changeMode/:id", function(req, res, next) {
-  res.send(req.params.id);
-  console.log(req.params.id);
+router.get('/pushButton', function(req, res, next) {
+  fb.on('value', function(dataSnapshot) {
+    switch (dataSnapshot.val().mode) {
+      case "orderFood":
+        orderFood(function(retVal) {
+          res.json(retVal);
+        });
+      break;
+      case "morning":
+        morning(function(retVal) {
+          res.json(retVal);
+        });
+      break;
+      default:
+        res.send("Booty");
+    }
+    fb.off();
+  });
+});
+
+// router.get('/morning', function(req, res, next) {
+//
+// });
+
+router.get("/changeMode/:id", function(req, res, next) {
+  fb.set({mode: req.params.id}, function(err) {
+    if (err) {
+      res.send(error);
+    } else {
+      res.send("booty");
+    }
+  });
+
 });
 
 module.exports = router;
